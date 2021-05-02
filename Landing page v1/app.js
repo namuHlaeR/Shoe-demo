be70b62ef81a2d37fb1009ffc61bc1e7316f1b09
namuHlaeR <namuhlaer@gmail.com>
2021-05-03 00:21:36 +0200

    Welcome page

--- Landing page v1/app.js
+++ Landing page v1/app.js
@@ -1,14 +1,39 @@
-const point = document.getElementById('two');
-const pointTxt = document.getElementById('twoTxt');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const oneTxt = document.getElementById('oneTxt');
+const twoTxt = document.getElementById('twoTxt');
+const threeTxt = document.getElementById('threeTxt');
+let dots = [one, two, three];
+let dotTxts = [ oneTxt, twoTxt, threeTxt];
 
+console.log(dotTxts);
+console.log(dots);
+
+
+/*
 point.addEventListener('mouseover', function nav(){
-    if(MouseEvent.apply){
+    if('mouseover'){
         pointTxt.classList.add('show')
     }
     setTimeout(function(){
         pointTxt.classList.remove('show')
-    }, 1000);
+    }, 1500);
 });
+*/
+
+function nav(){
+        dots.forEach(dot => {
+        dot.addEventListener('mouseover', function nav(){
+            if('mouseover'){
+                oneTxt.classList.add('show')
+            }
+            setTimeout(function(){
+                oneTxt.classList.remove('show')
+            }, 1500);
+        });      
+    });
+};
 
 nav();
 

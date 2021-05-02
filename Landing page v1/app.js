275f0a88a50de36ccc5e3da4793675f9bc8df2ed
namuHlaeR <namuhlaer@gmail.com>
2021-05-02 11:26:02 +0200

    Button animation

--- Landing page v1/app.js
+++ Landing page v1/app.js
@@ -1,3 +1,15 @@
 const point = document.getElementById('two');
+const pointTxt = document.getElementById('twoTxt');
+
+point.addEventListener('mouseover', function nav(){
+    if(MouseEvent.apply){
+        pointTxt.classList.add('show')
+    }
+    setTimeout(function(){
+        pointTxt.classList.remove('show')
+    }, 1000);
+});
+
+nav();
 
 console.log("Hello");
